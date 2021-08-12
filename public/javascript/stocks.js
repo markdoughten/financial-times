079e9c0ac03a0f44aa5063763be20392bf1c7eaf
markdoughten <you@example.com>
2021-08-12 17:08:11 -0400

    CORS install

--- public/javascript/stocks.js
+++ public/javascript/stocks.js
@@ -9,34 +9,26 @@ function removeSpaces(string) {
 }
 
 // Construct the table data based on the stock information, id attribute to identify right or left
-function makeBody(data, keys, id) {
+function makeBody(stocks, keys, id) {
     if(id === 1){
-    console.log(keys);
-    $(".stock-data-head-1 tr").remove();
-    $(".stock-data-body-1 tr").remove();
+    clearTable(1)
     const row = $("<tr class='stocks'>");
     $(".stock-data-head-1").append(row);
-    row.append($("<td class='stock-data-row'>" + keys[0] + "</td>"));
-    row.append($("<td class='stock-data-row'>" + data[keys[0]] + "</td>"));
+    appendRow(keys[0], stocks[keys[0]], row)
     for(var j = 1; j < keys.length; j++) {
         const row = $("<tr class='stocks'>");
         $(".stock-data-body-1").append(row);
-        row.append($("<td class='stock-data-row'>" + keys[j] + "</td>"));
-        row.append($("<td class='stock-data-row'>" + data[keys[j]] + "</td>"));
+        appendRow(keys[j], stocks[keys[j]], row)
     }
 } else {
-    console.log(keys);
-    $(".stock-data-head-2 tr").remove();
-    $(".stock-data-body-2 tr").remove();
+    clearTable(2)
     const row = $("<tr class='stocks'>");
     $(".stock-data-head-2").append(row);
-    row.append($("<td class='stock-data-row'>" + keys[0] + "</td>"));
-    row.append($("<td class='stock-data-row'>" + data[keys[0]] + "</td>"));
+    appendRow(keys[0], stocks[keys[0]], row)
     for(var j = 1; j < keys.length; j++) {
         const row = $("<tr class='stocks'>");
         $(".stock-data-body-2").append(row);
-        row.append($("<td class='stock-data-row'>" + keys[j] + "</td>"));
-        row.append($("<td class='stock-data-row'>" + data[keys[j]] + "</td>"));
+        appendRow(keys[j], stocks[keys[j]], row)
     }
     }
 }
@@ -51,31 +43,14 @@ $( "#stocks-1" ).submit(function(event) {
         url: getUrl + formValues,
         type: requestMethod,
         success: function(data){
-            if (data !== 0) {
-                const keys = Object.keys(data)
+            const keys = Object.keys(data)
+            //console.log(keys)
+            if (keys.length !== 0) {
                 makeBody(data, keys, 1);
             }
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            var errorMsg = 'Ajax request failed: ' + xhr.responseText;
-            $('#content').html(errorMsg);
-          }
-    });
-});
-
-//Ajax call for the left stock form
-$( "#stocks-2" ).submit(function(event) {
-  event.preventDefault();
-  let getUrl = $(this).attr("action");
-  let requestMethod = $(this).attr("method");
-  let formValues = $(this).serialize();
-      $.ajax({
-        url: getUrl + formValues,
-        type: requestMethod,
-        success: function(data){
-            if (data !== 0) {
-                const keys = Object.keys(data)
-                makeBody(data, keys, 2);
+            else {
+                clearTable(1)
+                errorDetected(1)
             }
         },
         error: function (xhr) {
@@ -84,3 +59,56 @@ $( "#stocks-2" ).submit(function(event) {
           }
     });
 });
+
+//Ajax call for the left stock form
+    $("#stocks-2").submit(function (event) {
+        event.preventDefault();
+        let getUrl = $(this).attr("action");
+        let requestMethod = $(this).attr("method");
+        let formValues = $(this).serialize();
+        $.ajax({
+            url: getUrl + formValues,
+            type: requestMethod,
+            success: function (data) {
+                const keys = Object.keys(data)
+                if (keys.length !== 0) {
+                    makeBody(data, keys, 2);
+                } else {
+                    clearTable(2)
+                    errorDetected(2)
+                }
+            },
+            error: function (xhr) {
+                var errorMsg = 'Ajax request failed: ' + xhr.responseText;
+                $('#content').html(errorMsg);
+            }
+        });
+    });
+
+    function appendRow(key, data, row) {
+        row.append($("<td class='stock-data-row'>" + key + "</td>"));
+        row.append($("<td class='stock-data-row'>" + data + "</td>"));
+    }
+
+    function errorDetected(body) {
+        if (body === 1) {
+            const row = $("<tr class='error'>");
+            $(".stock-data-head-1").append(row);
+            row.append($("<td class='error'>Stock not found. Please try again!</td>"));
+        } else {
+            const row = $("<tr class='error'>");
+            $(".stock-data-head-2").append(row);
+            row.append($("<td class='error'>Stock not found. Please try again!</td>"));
+        }
+    }
+
+    function clearTable(body) {
+        if (body === 1) {
+            $(".stock-data-head-1 tr").remove();
+            $(".stock-data-body-1 tr").remove();
+        } else {
+            $(".stock-data-head-2 tr").remove();
+            $(".stock-data-body-2 tr").remove();
+        }
+    }
+
