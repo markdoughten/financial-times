969d0b3c3e4daa25d5e5122b884238b5180c891b
Mark Doughten <doughtem@oregonstate.edu>
2022-06-03 19:11:20 -0400

    adding month and remaking tables

--- public/javascript/treasury-rates.js
+++ public/javascript/treasury-rates.js
@@ -1,35 +1,49 @@
 //Construct the table from the json data for the treasury rate table
 function makeTable(data) {
-            for(var j = 0; j < data.length; j++) {
-                if(j % 2 === 0){
-                    if(j === 0) {
-                        var row = $("<tr class='title'>");
-                        for (var i = 0; i < data[j].length; i++) {
-                            row.append($("<td class='category'>" + data[j][i] + "</td>"));
-                        }
-                    } else {
-                        var row = $("<tr class='even-rates'>");
-                        for(var i = 0; i < data[j].length; i++) {
-                            row.append($("<td class='category'>" + data[j][i] + "</td>"));
-                        }
-
-                    }
-                $(".treasury-rates").append(row);
+    for(var j = 0; j < data.length; j++) {
+        if(j % 2 === 0){
+            if(j === 0) {
+                var row = $("<tr class='title'>");
+                for (var i = 0; i < data[j].length; i++) {
+                    row.append($("<td class='category'>" + data[j][i] + "</td>"));
                 }
-                else {
-                var row = $("<tr class='odd-rates'>");
+            } else {
+                var row = $("<tr class='even-rates'>");
                 for(var i = 0; i < data[j].length; i++) {
                     row.append($("<td class='category'>" + data[j][i] + "</td>"));
                 }
-                $(".treasury-rates").append(row);
-                }
+
             }
+        $(".treasury-rates").append(row);
+        }
+        else {
+        var row = $("<tr class='odd-rates'>");
+        for(var i = 0; i < data[j].length; i++) {
+            row.append($("<td class='category'>" + data[j][i] + "</td>"));
+        }
+        $(".treasury-rates").append(row);
+        }
+    }
 }
-
-
 
 //When the page has loaded.
 $( document ).ready(function(){
+
+    //Set constraints on the search function
+    const currentDate = new Date();
+    const month = currentDate.getMonth() + 1;
+    const year = currentDate.getFullYear()
+    const input = year + '-0' + month;
+
+    if (month >= 10){
+         let input = year + '-' + month;
+    }
+
+    $('#treasury-month').attr({
+        value: input,
+        max: input
+    });
+
     //Perform Ajax request.
     $.ajax({
         url: '/treasury-scraper',
@@ -44,4 +58,28 @@ $( document ).ready(function(){
             $('#content').html(errorMsg);
           }
     });
+});
+
+$(document).ready(function () {
+  $("#monthly-treasury").submit(function (event) {
+    event.preventDefault();
+    var formData = {
+      month: $("#treasury-month").val(),
+    };
+
+    $.ajax({
+      type: "post",
+      url: "/treasury-monthly",
+      data: formData,
+      dataType: "json",
+      encode: true,
+    }).done(function (data) {
+        if(data !== 0) {
+            $('.title').remove();
+            $('.odd-rates').remove();
+            $('.even-rates').remove();
+            makeTable(data.data);
+        }
+    });
+  });
 });
